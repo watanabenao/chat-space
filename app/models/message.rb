e8bf65811cfd6d279b979fa3c867d71d437264b1
watanabenao <watanabenao@watanabenoMacBook-puro.local>
2018-12-18 15:41:25 +0900

    Message function edit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :group
-  belongs_to :user
+  belongs_to :user 
+
+  validates :body, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
+
+  
 end
