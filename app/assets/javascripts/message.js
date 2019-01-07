3ac4647f7fa50325248a38fb35745b46c415ccdc
watanabenao <watanabenao@watanabenoMacBook-puro.local>
2019-01-07 13:14:38 +0900

    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,11 @@
-$(function(){
+$(document).on('turbolinks:load',function(){
   function buildHTML(message){
     var image = (message.image === null) ? "" : `<img src="${message.image}" class="lower-message__image">`
-    var html = `<div class="chat-main__message">      
+    var html = `<div class="chat-main__message" data-message-id= "${message.id}">      
                   <p class="chat-main__message-name">
                     ${message.user_name}
                   </p>
-                  <p class="chat-main__message-time">s
+                  <p class="chat-main__message-time">
                     ${message.created_at}
                   </p>
                   <p class="main-message-text">
@@ -47,4 +47,27 @@ $(function(){
       $(".form__submit").prop('disabled', false)
     })
   });
+
+  var interval = setInterval(function(){
+    if(window.location.href.match(/\/groups\/\d+\/messages/)){
+      var id = $('.chat-main__message:last').data('message-id')
+      $.ajax({
+        url: location.href,
+        type: 'GET',  
+        dataType: 'json',
+        data: {id: id}
+      })
+      .done(function(messages){
+        messages.forEach(function(message){
+         $(".chat-main__list").append(buildHTML(message));
+         });
+         $(".chat-main__content").animate({scrollTop: $(".chat-main__content")[0].scrollHeight}, 'fast');
+      })
+      .fail(function(data){
+        alert('自動更新に失敗しました')
+      })
+    }else{
+      clearInterval(interval);
+    }}, 5000);
+
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@
   json.image @message.image.url
   json.user_name @message.user.name
   json.created_at @message.created_at.strftime('%Y/%m/%d %H:%M')
+  json.id @message.id
